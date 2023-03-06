2e9a85a2a6c3404cc96ecb31edc34fcc61263232
fedeh2021 <fedejhirsch@gmail.com>
2023-03-06 19:02:55 -0300

    mywork + mockdata

--- src/components/MyWork.js
+++ src/components/MyWork.js
@@ -1,13 +1,28 @@
 import React from 'react'
-import { makeStyles, Typography } from '@material-ui/core'
-
+import { Card, CardContent, CardMedia, Grid, Link, makeStyles, Typography } from '@material-ui/core'
+import { mockData } from '../mockData'
 
 const MyWork = ({ title, dark, id }) => {
   const classes = useStyles()
-
   return (
     <div className={`${classes.section} ${dark && classes.sectionDark}`}><div className={classes.sectionContent} id={id}>
       <Typography variant='h3'>{title}</Typography>
+      <Grid container className={classes.gridContainer}>
+        {
+          mockData.map(({ title, image, link }, index) => (
+            <Grid item key={index} xs={12} sm={6} md={4}>
+              <Card className={classes.card} >
+                <CardMedia image={image} className={classes.cover} title='cover' />
+                <CardContent>
+                  <Link href={link} color='primary' target='_blank' rel='noopener noreferrer'>
+                    {title}
+                  </Link>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))
+        }
+      </Grid>
     </div>
     </div>
   )
@@ -24,7 +39,20 @@ const useStyles = makeStyles((theme) => ({
   sectionContent: {
     maxWidth: "80vw",
     margin: '0 auto',
-  }
+    padding: theme.spacing(5),
+  },
+  gridContainer: {
+    marginTop: theme.spacing(10),
+  },
+  card: {
+    maxWidth: 345,
+    minHeight: 275,
+    margin: theme.spacing(3),
+  },
+  cover: {
+    height: 0,
+    paddingTop: '56.25%',
+  },
 }))
 
 export default MyWork
---
+++ src/mockData.js
@@ -0,0 +1,29 @@
+import peruCaos from '../src/images/reaparecen-denuncias-de-violaciones-de-derechos-humanos-dentro-de-portales-de-transmilenio-995792.jpg'
+
+export const mockData = [
+    {
+        title: 'peru, caos',
+        image: peruCaos,
+        link: 'https://eleconomista.com.ar/internacional/el-2023-arranco-zona-andina-atravesada-violencia-n59708',
+    },
+    {
+        title: 'peru, caos',
+        image: peruCaos,
+        link: 'https://eleconomista.com.ar/internacional/el-2023-arranco-zona-andina-atravesada-violencia-n59708',
+    },
+    {
+        title: 'peru, caos',
+        image: peruCaos,
+        link: 'https://eleconomista.com.ar/internacional/el-2023-arranco-zona-andina-atravesada-violencia-n59708',
+    },
+    {
+        title: 'peru, caos',
+        image: peruCaos,
+        link: 'https://eleconomista.com.ar/internacional/el-2023-arranco-zona-andina-atravesada-violencia-n59708',
+    },
+    {
+        title: 'titulo de la nota y foto de la nota',
+        image: peruCaos,
+        link: 'https://eleconomista.com.ar/internacional/el-2023-arranco-zona-andina-atravesada-violencia-n59708',
+    },
+]
