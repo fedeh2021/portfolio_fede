e20ed22afd676ce1d4418598bb38262ae4634d04
Federico Hirsch Veloso <fhirsch@onfocusdev.com>
2023-03-28 18:42:16 -0300

    update notas

--- src/noticeData.js
+++ src/noticeData.js
@@ -1,35 +1,43 @@
-import zonaAndina from '../src/images/violencia_andina.jpg'
-import brasilToma from '../src/images/brasil_toma_congreso.jpg'
-import protestasPeru from '../src/images/protestas_peru.jpg'
-import maduroPetro from '../src/images/maduro_y_petro.jpg'
+import zonaAndina from "../src/images/violencia_andina.jpg";
+import brasilToma from "../src/images/brasil_toma_congreso.jpg";
+import protestasPeru from "../src/images/protestas_peru.jpg";
+import maduroPetro from "../src/images/maduro_y_petro.jpg";
+import fotoCumbre from "../src/images/foto_cumbre.jpg";
 
 export const noticeData = [
-    {
-        title: 'Crisis en Brasil: cuando ganar una elección no genera legitimidad',
-        image: brasilToma,
-        link: 'https://www.pressreader.com/argentina/el-economista-argentina/20230112/281736978556028',
-        date: '12/01/2023',
-        newspaper: 'El Economista',
-    },
-    {
-        title: 'Perú: tras el autogolpe, caos',
-        image: protestasPeru,
-        link: 'https://eleconomista.com.ar/internacional/peru-tras-autogolpe-caos-n59441',
-        date: '01/02/2023',
-        newspaper: 'El Economista',
-    },
-    {
-        title: 'El 2023 arrancó con la zona andina atravesada por la violencia',
-        image: zonaAndina,
-        link: 'https://eleconomista.com.ar/internacional/el-2023-arranco-zona-andina-atravesada-violencia-n59708',
-        date: '12/02/2023',
-        newspaper: 'El Economista',
-    },
-    {
-        title: 'Venezuela y Colombia enfrentan un complicado comienzo de año',
-        image: maduroPetro,
-        link: 'https://eleconomista.com.ar/internacional/venezuela-colombia-enfrentan-complicado-comienzo-ano-n60123',
-        date: '28/02/2023',
-        newspaper: 'El Economista',
-    },
-]
+  {
+    title: "Crisis en Brasil: cuando ganar una elección no genera legitimidad",
+    image: brasilToma,
+    link: "https://www.pressreader.com/argentina/el-economista-argentina/20230112/281736978556028",
+    date: "12/01/2023",
+    newspaper: "El Economista",
+  },
+  {
+    title: "Perú: tras el autogolpe, caos",
+    image: protestasPeru,
+    link: "https://eleconomista.com.ar/internacional/peru-tras-autogolpe-caos-n59441",
+    date: "01/02/2023",
+    newspaper: "El Economista",
+  },
+  {
+    title: "El 2023 arrancó con la zona andina atravesada por la violencia",
+    image: zonaAndina,
+    link: "https://eleconomista.com.ar/internacional/el-2023-arranco-zona-andina-atravesada-violencia-n59708",
+    date: "12/02/2023",
+    newspaper: "El Economista",
+  },
+  {
+    title: "Venezuela y Colombia enfrentan un complicado comienzo de año",
+    image: maduroPetro,
+    link: "https://eleconomista.com.ar/internacional/venezuela-colombia-enfrentan-complicado-comienzo-ano-n60123",
+    date: "28/02/2023",
+    newspaper: "El Economista",
+  },
+  {
+    title: "De las relaciones internacionales a relaciones interpersonales",
+    image: fotoCumbre,
+    link: "https://eleconomista.com.ar/internacional/de-relaciones-internacionales-relaciones-interpersonales-n60675",
+    date: "17/03/2023",
+    newspaper: "El Economista",
+  },
+];
