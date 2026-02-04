6ab8dc96f04d685e76e077956f034aafe0081fe5
Federico Hirsch Veloso <fedejhirsch@gmail.com>
2026-02-04 19:45:40 -0300

    cambio en images

---
+++ src/images.js
@@ -0,0 +1,73 @@
+import zonaAndina from "./images/violencia_andina.jpg";
+import brasilToma from "./images/brasil_toma_congreso.jpg";
+import protestasPeru from "./images/protestas_peru.jpg";
+import maduroPetro from "./images/maduro_y_petro.jpg";
+import fotoCumbre from "./images/foto_cumbre.jpg";
+import cumbreIberoamericana from "./images/cumbre_iberoamericana.jpg";
+import petroMano from "./images/petroMano.jpg";
+import paraguayPres from "./images/paraguayPres.jpg";
+import renunciaLasso from "./images/renunciaLasso.jpg";
+import dinaLasso from "./images/dinaLasso.jpg";
+import cumbreMercosur from "./images/cumbreMercosur.jpg";
+import cumbreBrasil from "./images/cumbreBrasil.jpg";
+import peruAgosto from "./images/peruAgosto.jpg";
+import eleccionesEcuador from "./images/eleccionesEcuador.jpg";
+import fronteraGuyana from "./images/fronteraGuyana.jpg";
+import bukele from "./images/bukele.jpg";
+import bukeleDictador from "./images/bukeleDictador.jpg";
+import venezuelaCaos from "./images/venezuelaCaos.jpg";
+import panamaMulino from "./images/panamaMulino.jpg";
+import lulaChina from "./images/lulaChina.jpg";
+import asambleaGeneral from "./images/asambleaGeneral.jpg";
+import eleccionesUruguay from "./images/eleccionesUruguay.png";
+import g20Brasil from "./images/g20Brasil.png";
+import resumen2024 from "./images/resumen2024.png";
+import elecciones2025 from "./images/elecciones2025.png";
+import migracionTrump from "./images/migracionTrump.jpg";
+import primariasUruguay from "./images/primariasUruguay.jpg";
+import eleccionesVenezuela from "./images/eleccionesVenezuela.jpg";
+import bolSinEvo from "./images/bolSinEvo.jpg";
+import presOnu from "./images/presOnu.png";
+import caidaDina from "./images/caidaDina.png";
+import banderaEcuador from "./images/banderaEcuador.jpg";
+import colombiaPetro from "./images/colombiaPetro.jpg";
+import esequiboMaduro from "./images/esequiboMaduro.jpg";
+import vicepresidentes from "./images/vicepresidentes.jpg";
+
+export const images = {
+  zonaAndina,
+  brasilToma,
+  protestasPeru,
+  maduroPetro,
+  fotoCumbre,
+  cumbreIberoamericana,
+  petroMano,
+  paraguayPres,
+  renunciaLasso,
+  dinaLasso,
+  cumbreMercosur,
+  cumbreBrasil,
+  peruAgosto,
+  eleccionesEcuador,
+  fronteraGuyana,
+  bukele,
+  bukeleDictador,
+  venezuelaCaos,
+  panamaMulino,
+  lulaChina,
+  asambleaGeneral,
+  eleccionesUruguay,
+  g20Brasil,
+  resumen2024,
+  elecciones2025,
+  migracionTrump,
+  primariasUruguay,
+  eleccionesVenezuela,
+  bolSinEvo,
+  presOnu,
+  caidaDina,
+  banderaEcuador,
+  colombiaPetro,
+  esequiboMaduro,
+  vicepresidentes
+};
--- src/noticeData.js
+++ src/noticeData.js
@@ -1,280 +1,246 @@
-import zonaAndina from "../src/images/violencia_andina.jpg";
-import brasilToma from "../src/images/brasil_toma_congreso.jpg";
-import protestasPeru from "../src/images/protestas_peru.jpg";
-import maduroPetro from "../src/images/maduro_y_petro.jpg";
-import fotoCumbre from "../src/images/foto_cumbre.jpg";
-import cumbreIberoamericana from "../src/images/cumbre_iberoamericana.jpg";
-import petroMano from "../src/images/petroMano.jpg";
-import paraguayPres from "../src/images/paraguayPres.jpg";
-import renunciaLasso from '../src/images/renunciaLasso.jpg';
-import dinaLasso from '../src/images/dinaLasso.jpg';
-import cumbreMercosur from '../src/images/cumbreMercosur.jpg';
-import cumbreBrasil from '../src/images/cumbreBrasil.jpg';
-import peruAgosto from '../src/images/peruAgosto.jpg';
-import eleccionesEcuador from '../src/images/eleccionesEcuador.jpg';
-import fronteraGuyana from '../src/images/fronteraGuyana.jpg';
-import bukele from '../src/images/bukele.jpg';
-import bukeleDictador from '../src/images/bukeleDictador.jpg';
-import venezuelaCaos from '../src/images/venezuelaCaos.jpg';
-import panamaMulino from '../src/images/panamaMulino.jpg';
-import lulaChina from '../src/images/lulaChina.jpg';
-import asambleaGeneral from '../src/images/asambleaGeneral.jpg';
-import eleccionesUruguay from '../src/images/eleccionesUruguay.png';
-import g20Brasil from '../src/images/g20Brasil.png';
-import resumen2024 from '../src/images/resumen2024.png';
-import elecciones2025 from '../src/images/elecciones2025.png';
-import migracionTrump from '../src/images/migracionTrump.jpg';
-import primariasUruguay from '../src/images/primariasUruguay.jpg';
-import eleccionesVenezuela from '../src/images/eleccionesVenezuela.jpg';
-import bolSinEvo from '../src/images/bolSinEvo.jpg';
-import presOnu from '../src/images/presOnu.png';
-import caidaDina from '../src/images/caidaDina.png';
-import banderaEcuador from '../src/images/banderaEcuador.jpg';
-import colombiaPetro from '../src/images/colombiaPetro.jpg';
-import esequiboMaduro from '../src/images/esequiboMaduro.jpg';
-import vicepresidentes from '../src/images/vicepresidentes.jpg';
+import { images } from '../src/images';
 
 export const noticeData = [
   {
     title: "Crisis en Brasil: cuando ganar una elección no genera legitimidad",
-    image: brasilToma,
+    image: images.brasilToma,
     link: "https://www.pressreader.com/argentina/el-economista-argentina/20230112/281736978556028",
     date: "12/01/2023",
     newspaper: "El Economista",
   },
   {
     title: "Perú: tras el autogolpe, caos",
-    image: protestasPeru,
+    image: images.protestasPeru,
     link: "https://eleconomista.com.ar/internacional/peru-tras-autogolpe-caos-n59441",
     date: "01/02/2023",
     newspaper: "El Economista",
   },
   {
     title: "El 2023 arrancó con la zona andina atravesada por la violencia",
-    image: zonaAndina,
+    image: images.zonaAndina,
     link: "https://eleconomista.com.ar/internacional/el-2023-arranco-zona-andina-atravesada-violencia-n59708",
     date: "12/02/2023",
     newspaper: "El Economista",
   },
   {
     title: "Venezuela y Colombia enfrentan un complicado comienzo de año",
-    image: maduroPetro,
+    image: images.maduroPetro,
     link: "https://eleconomista.com.ar/internacional/venezuela-colombia-enfrentan-complicado-comienzo-ano-n60123",
     date: "28/02/2023",
     newspaper: "El Economista",
   },
   {
     title: "De las relaciones internacionales a relaciones interpersonales",
-    image: fotoCumbre,
+    image: images.fotoCumbre,
     link: "https://eleconomista.com.ar/internacional/de-relaciones-internacionales-relaciones-interpersonales-n60675",
     date: "17/03/2023",
     newspaper: "El Economista",
   },
   {
     title: "Cumbre Iberoamericana: ¿Una nueva oportunidad perdida?",
-    image: cumbreIberoamericana,
+    image: images.cumbreIberoamericana,
     link: "https://www.perfil.com/noticias/opinion/cumbre-iberoamericana-una-nueva-oportunidad-perdida.phtml",
     date: "07/04/2023",
     newspaper: "Perfil",
   },
   {
     title: 'Las reformas tributarias condicionan a los presidentes latinoamericanos',
-    image: petroMano,
+    image: images.petroMano,
     link: 'https://www.perfil.com/noticias/internacional/las-reformas-tributarias-condicionan-a-los-presidentes-latinoamericanos.phtml',
     date: '22/04/2023',
     newspaper: 'Perfil',
   },
   {
     title: 'Paraguay: ¿continuidad o cambio?',
-    image: paraguayPres,
+    image: images.paraguayPres,
     link: 'https://www.perfil.com/noticias/internacional/paraguay-continuidad-o-cambio.phtml',
     date: '30/04/2023',
     newspaper: 'Perfil',
   },
   {
     title: 'La inestabilidad andina',
-    image: dinaLasso,
+    image: images.dinaLasso,
     link: 'https://www.perfil.com/noticias/opinion/la-inestabilidad-andina.phtml',
     date: '13/05/2023',
     newspaper: 'Perfil',
   },
   {
     title: 'Espejo de la inestabilidad latinoamericana',
-    image: renunciaLasso,
+    image: images.renunciaLasso,
     link: 'https://www.perfil.com/noticias/opinion/espejo-de-la-inestabilidad-latinoamericana.phtml',
     date: '20/05/2023',
     newspaper: 'Perfil',
   },
   {
     title: 'Cumbre en Brasil: nada nuevo bajo el sol',
-    image: cumbreBrasil,
+    image: images.cumbreBrasil,
     link: 'https://www.perfil.com/noticias/opinion/cumbre-en-brasil-nada-nuevo-bajo-el-sol.phtml',
     date: '03/06/2023',
     newspaper: 'Perfil',
   },
   {
     title: 'Mercosur: la misma historia de siempre',
-    image: cumbreMercosur,
+    image: images.cumbreMercosur,
     link: 'https://www.perfil.com/noticias/opinion/mercosur-la-misma-historia-de-siempre.phtml',
     date: '08/07/2023',
     newspaper: 'Perfil',
   },
   {
     title: 'Entre el caos y la permanencia',
-    image: peruAgosto,
+    image: images.peruAgosto,
     link: 'https://www.perfil.com/noticias/opinion/entre-el-caos-y-la-permanencia.phtml',
     date: '29/07/2023',
     newspaper: 'Perfil',
   },
   {
     title: 'Ecuador concurre a las urnas entre la inseguridad y la incertidumbre',
-    image: eleccionesEcuador,
+    image: images.eleccionesEcuador,
     link: 'https://eleconomista.com.ar/internacional/ecuador-concurre-urnas-inseguridad-incertidumbre-n67226',
     date: '12/10/2023',
     newspaper: 'El Economista',
   },
   {
     title: 'Qué hay detrás del conflicto',
-    image: fronteraGuyana,
+    image: images.fronteraGuyana,
     link: 'https://www.perfil.com/noticias/internacional/que-hay-detras-del-conflicto.phtml',
     date: '09/12/2023',
     newspaper: 'Perfil',
   },
   {
     title: 'El Caribe y Centroamérica, a elecciones',
-    image: bukele,
+    image: images.bukele,
     link: 'https://www.perfil.com/noticias/opinion/el-caribe-y-centroamerica-a-elecciones.phtml',
     date: '20/01/24',
     newspaper: 'Perfil',
   },
   {
     title: '(Mención) El dilema Salvador',
-    image: bukeleDictador,
+    image: images.bukeleDictador,
     link: 'https://www.perfil.com/noticias/columnistas/el-dilema-salvador.phtml',
     date: '04/02/24',
     newspaper: 'Perfil',
   },
   {
     title: 'Semanas oscuras de la democracia venezolana',
-    image: venezuelaCaos,
+    image: images.venezuelaCaos,
     link: 'https://www.perfil.com/noticias/internacional/semanas-oscuras-de-la-democracia-venezolana.phtml',
     date: '23/02/24',
     newspaper: 'Perfil',
   },
   {
     title: 'América Latina: la ilegalidad al poder',
-    image: panamaMulino,
+    image: images.panamaMulino,
     link: 'https://www.perfil.com/noticias/internacional/america-latina-la-ilegalidad-al-poder.phtml',
     date: '04/05/24',
     newspaper: 'Perfil',
   },
   {
     title: 'América Latina made in China',
-    image: lulaChina,
+    image: images.lulaChina,
     link: 'https://www.perfil.com/noticias/internacional/america-latina-made-in-china.phtml',
     date: '18/05/24',
     newspaper: 'Perfil',
   },
   {
     title: 'Primarias en el oasis democrático de América Latina',
-    image: primariasUruguay,
+    image: images.primariasUruguay,
     link: 'https://www.perfil.com/noticias/opinion/primarias-en-el-oasis-democratico-de-america-latina.phtml',
     date: '30/06/24',
     newspaper: 'Perfil',
   },
   {
     title: 'No es cualquier país',
-    image: eleccionesVenezuela,
+    image: images.eleccionesVenezuela,
     link: 'https://www.perfil.com/noticias/internacional/no-es-cualquier-pais.phtml',
     date: '27/07/24',
     newspaper: 'Perfil',
   },
   {
     title: 'Pasó la Asamblea de las Naciones Unidas: ¿qué queda para América Latina?',
-    image: asambleaGeneral,
+    image: images.asambleaGeneral,
     link: 'https://iky.b07.myftpupload.com/politica/paso-la-asamblea-de-las-naciones-unidas-que-queda-para-america-latina/',
     date: '15/10/24',
     newspaper: 'El Gobierno',
   },
   {
     title: 'Elecciones en Uruguay: Todo lo que tenes que saber',
-    image: eleccionesUruguay,
+    image: images.eleccionesUruguay,
     link: 'https://iky.b07.myftpupload.com/politica/elecciones-en-uruguay-todo-lo-que-tenes-que-saber/',
     date: '26/10/24',
     newspaper: 'El Gobierno',
   },
   {
     title: 'Lo que dejo el G20 para América Latina',
-    image: g20Brasil,
+    image: images.g20Brasil,
     link: 'https://iky.b07.myftpupload.com/politica/lo-que-dejo-el-g20-para-america-latina/',
     date: '01/12/24',
     newspaper: 'El Gobierno',
   },
   {
     title: '2024 en América Latina: elecciones, crisis y el desafío democrático',
-    image: resumen2024,
+    image: images.resumen2024,
     link: 'https://iky.b07.myftpupload.com/mundo/2024-en-america-latina-elecciones-crisis-y-el-desafio-democratico/',
     date: '01/01/25',
     newspaper: 'El Gobierno',
   },{
     title: 'América Latina en 2025: elecciones en la región de las turbulencias constantes',
-    image: elecciones2025,
+    image: images.elecciones2025,
     link: 'https://iky.b07.myftpupload.com/politica/america-latina-en-2025-elecciones-en-la-region-de-las-turbulencias-constantes/',
     date: '08/01/25',
     newspaper: 'El Gobierno',
   },
   {
     title: 'Donald Trump endurece la política migratoria en su regreso a la Casa Blanca',
-    image: migracionTrump,
+    image: images.migracionTrump,
     link: 'https://iky.b07.myftpupload.com/mundo/donald-trump-endurece-la-politica-migratoria-en-su-regreso-a-la-casa-blanca/',
     date: '19/02/25',
     newspaper: 'El Gobierno',
   },
   {
     title: 'Ecuador elige presidente entre la violencia y el posible regreso del correísmo',
-    image: banderaEcuador,
+    image: images.banderaEcuador,
     link: 'https://diarioelgobierno.ar/noticia/413-ecuador-elige-presidente-entre-la-violencia-y-el-posible-regreso-del-correismo',
     date: '12/04/25',
     newspaper: 'El Gobierno',
   },
   {
     title: 'Venezuela desafía a la Corte Internacional de Justicia y profundiza el conflicto por el Esequibo',
-    image: esequiboMaduro,
+    image: images.esequiboMaduro,
     link: 'https://diarioelgobierno.ar/noticia/150-venezuela-desafia-a-la-corte-internacional-de-justicia-y-profundiza-el-conflicto-por-el-esequibo',
     date: '23/05/25',
     newspaper: 'El Gobierno',
   },
   {
     title: 'Entre la consulta y las balas: la Colombia polarizada de Petro',
-    image: colombiaPetro,
+    image: images.colombiaPetro,
     link: 'https://diarioelgobierno.ar/noticia/209-entre-la-consulta-y-las-balas-la-colombia-polarizada-de-petro',
     date: '25/06/25',
     newspaper: 'El Gobierno',
   },
   {
     title: 'Vicepresidentes: ¿piezas clave o bombas de tiempo del Poder Ejecutivo?',
-    image: vicepresidentes,
+    image: images.vicepresidentes,
     link: 'https://diarioelgobierno.ar/noticia/251-vicepresidentes-piezas-clave-o-bombas-de-tiempo-del-poder-ejecutivo',
     date: '24/07/25',
     newspaper: 'El Gobierno',
   },
   {
     title: 'Bolivia, a las urnas sin Evo ni Arce: una elección huérfana que definirá la década',
-    image: bolSinEvo,
+    image: images.bolSinEvo,
     link: 'https://diarioelgobierno.ar/noticia/280-bolivia-a-las-urnas-sin-evo-ni-arce-una-eleccion-huerfana-que-definira-la-decada',
     date: '15/08/25',
     newspaper: 'El Gobierno',
   },
   {
     title: '¿Qué nos deja a América Latina esta Asamblea de la ONU?',
-    image: presOnu,
+    image: images.presOnu,
     link: 'https://diarioelgobierno.ar/noticia/359-que-nos-deja-a-america-latina-esta-asamblea-de-la-onu',
     date: '26/09/25',
     newspaper: 'El Gobierno',
   },
   {
     title: 'La caída de Dina Boluarte: Perú vuelve a tropezar con su propia inestabilidad',
-    image: caidaDina,
+    image: images.caidaDina,
     link: 'https://diarioelgobierno.ar/noticia/383-la-caida-de-dina-boluarte-peru-vuelve-a-tropezar-con-su-propia-inestabilidad',
     date: '09/10/25',
     newspaper: 'El Gobierno',
